9a53657b8b54ec8904a77eef7acc734715a2833d
Allan Chadwick <4chadwick@gmail.com>
2021-04-12 13:12:53 -0700

    wip

--- sample/pages/Jokes/_id/index.vue
+++ sample/pages/Jokes/_id/index.vue
@@ -1,33 +1,30 @@
 <template>
-  <div>Single Joke: {{ joke}}</div>
+  <div>Single Joke: {{ joke }}</div>
 </template>
 
-<script>
-import axios from 'axios';
+<script lang="ts">
+import axios, { AxiosResponse } from 'axios'
 
 export default {
-    data(){
-        return {
-            joke:{}
-        }
-    },
-    async created(){
-const config = {
+  data() {
+    return {
+      joke: {},
+    }
+  },
+  async created() {
+    const config = {
       headers: {
         Accept: 'application/json',
       },
     }
-try {
-    const str = `https://icanhazdadjoke.com/j/${this.$route.params.id}`;
-    console.log(str);
-    const res = await axios.get(str, config)
-    this.joke = res.data.joke;
-    console.log(joke);
-    
-} catch (err) {
-    console.log(err);
-}
-  
+    try {
+      const str = `https://icanhazdadjoke.com/j/${this.$route.params.id}`
+      console.log(str)
+      const res = await axios.get(str, config)
+      this.joke = res.data.joke
+    } catch (err) {
+      console.log(err)
     }
+  },
 }
 </script>
